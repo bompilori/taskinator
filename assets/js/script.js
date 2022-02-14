e5c3b789d8b9cbaaa9d8efbd79ea77e4c67d6067
santiago.hurtado314@gmail.com <santiago.hurtado314@gmail.com>
2022-02-13 23:07:22 -0500

    add ability to submit custom tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,8 @@ var taskFormHandler = function(event){
         alert("You need to fill out the task form!");
         return false;
     }
+    
+    formEl.reset();
 
     // package up data as an object
     var taskDataObj = {
@@ -19,7 +21,7 @@ var taskFormHandler = function(event){
         type: taskTypeInput
     };
     
-    formEl.reset();
+    
 
     // send it as an argument to createTaskEl
     createTaskEl(taskDataObj);

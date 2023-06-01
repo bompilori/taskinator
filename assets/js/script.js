11ab8f2c366956ab549557650450313d4d43b829
santiago.hurtado314@gmail.com <santiago.hurtado314@gmail.com>
2023-06-01 00:12:18 -0400

    event preventDefault

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function(){
+var createTaskHandler = function(event){
+    event.preventDefault();//prevent the browser reload every time a event is triggered 
+
     var listItemEl = document.createElement("li"); //create a new task item
     listItemEl.className = "task-item";//style the new task item
     listItemEl.textContent = "this is a new task";//add the text
     tasksToDoEl.appendChild(listItemEl);//append this element to the task list.
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
